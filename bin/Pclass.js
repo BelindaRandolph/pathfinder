faae8ce69e489ebba346f98e72e6b72314911f88
Belinda Randolph <belinda.a.randolph@jpl.nasa.gov>
2020-02-26 09:54:09 -0800

    addFeat

--- bin/Pclass.js
+++ bin/Pclass.js
@@ -1,61 +1,64 @@
-function Pclass(name, level) {
+
+//  Pclass.prototype.BaseSave = function ( level, good) {
+//          if (good == 1) { 
+//              x = 2
+//              y = 2}
+//          else if (good == 0) {
+//              x = 3
+//              y = 0}
+//  
+//          return Math.floor (level / x) + y
+//  }
+
+function Pclass(name, level, BaseMod, GoodBadFortitude, GoodBadReflex, GoodBadWill) {
     this.name = name;
     this.level = level;
+    this.NumFeats = Math.ceil (level/2)
+    this.Feats = []
+    // console.log ("name is " + name)
+    // console.log ("level is " + level)
+    // console.log (GoodBadFortitude, GoodBadReflex, GoodBadWill)
 
-    if (this.name == "Alchemist" ) 
-    {
-    this.BaseMod = 0.75
-    this.GoodBadFortitude = 1
-    this.GoodBadReflex= 1
-    this.GoodBadWill= 0
+    x = [ Math.floor(BaseMod * level),
+                            Math.max (0, Math.floor(BaseMod * level) -5),
+                            Math.max (0, Math.max (0, Math.floor(BaseMod * level) -5) - 5)]
+
+    this.BaseAttackBonus = [ Math.floor(BaseMod * level), 
+                            Math.max (0, Math.floor(BaseMod * level) -5),  
+                            Math.max (0, Math.max (0, Math.floor(BaseMod * level) -5) - 5)]
+
+
+    this.addFeat = function (featstr) {
+        if ( this.Feats.length < this.NumFeats ) {
+            this.Feats.push (featstr)
+        }
     }
-    else if (this.name == "WarPriest" ) 
-    {
-    this.BaseMod = 0.75
-    this.GoodBadFortitude = 1
-    this.GoodBadReflex= 0
-    this.GoodBadWill= 1
-    }
 
-    this.BaseAttackBonus = [ Math.floor(this.BaseMod * level), 
-                            Math.max (0, Math.floor(this.BaseMod * level) -5),  
-                            Math.max (0, Math.max (0, Math.floor(this.BaseMod * level) -5) - 5)]
-    this.FortitudeBaseSave = this.BaseSave (this.level, this.GoodBadFortitude)
-    this.ReflexBaseSave = this.BaseSave (this.level, this.GoodBadReflex)
-    this.WillBaseSave = this.BaseSave (this.level, this.GoodBadWill)
-
-
-}
-
-// This is where I put all my prototype functions
-Pclass.prototype = {
-    constructor: Pclass,
-
-    BaseSave:function (level, good ) {
-        if (good == 1) { 
+    BaseSave = function ( level, good) {
+        if (good == 1) {
             x = 2
-            y = 2}
+            y = 2
+        }
         else if (good == 0) {
             x = 3
             y = 0}
 
         return Math.floor (level / x) + y
     }
-    //
-    // Still need FortitudeSave, ReflexSave, and WillSave which are function of
-    // above and AbilityModifiers (CONmod, DEXmod, WISmod)
-    // and these are implemented based on my character traits.
 
+    this.FortitudeBaseSave = BaseSave (this.level, GoodBadFortitude)
+    this.ReflexBaseSave = BaseSave (this.level, GoodBadReflex)
+    this.WillBaseSave = BaseSave (this.level, GoodBadWill)
 }
 
 
 
-Alchemist = new Pclass ("Alchemist", 8 )
+// me = new Pclass ("Alchemist", 8 , 0.75, 1, 1, 0)
+// me.addFeat("PointBlankShot")
 
-console.log(Alchemist.name)
-console.log(Alchemist.level)
-console.log('F = ' + Alchemist.FortitudeBaseSave)
-console.log('R = ' + Alchemist.ReflexBaseSave)
-console.log('W = ' + Alchemist.WillBaseSave)
-console.log('BaseAttackBonus = ' + Alchemist.BaseAttackBonus)
+// console.log(me.name)
+// console.log('Feats = ' + me.Feats)
+// console.log('R = ' + me.ReflexBaseSave)
+// console.log('W = ' + me.WillBaseSave)
+// console.log('BaseAttackBonus = ' + me.BaseAttackBonus)
 
